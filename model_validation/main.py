80e9ba631b75691477da946b0ace7e5bfc3e144f
Sriram Mahendran <sriram2210617@ssn.edu.in>
2025-08-19 22:31:00 +0530

    Add files via upload

---
+++ model_validation/main.py
@@ -0,0 +1,7 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
+import joblib
+
+
+
